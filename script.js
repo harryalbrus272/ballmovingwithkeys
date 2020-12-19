c4df6dd9914f4b866391741ae9c3857d2279cc2f
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2020-12-19 18:57:39 +0530

    added arrow button functionality

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ window.addEventListener('keydown', (event) => {
   var key = event.key;
   if (true){
     console.log("inside if-block");
-    if ((key == "d" || key == "D") && position.right <= window.innerWidth-10) {
+    if ((key == "d" || key == "D" || key=="ArrowRight") && position.right <= window.innerWidth-10) {
       let n = parseInt(left) + moveBy + "px";
       ball.style.left = n;
-    } else if ((key == 's' || key == 'S') && position.bottom <= window.innerHeight-10) {
+    } else if ((key == 's' || key == 'S' || key=="ArrowDown") && position.bottom <= window.innerHeight-10) {
       let n = parseInt(top) + moveBy + "px";
       ball.style.top = n;
-    } else if ((key == 'a' || key == 'A') && position.left >= 10) {
+    } else if ((key == 'a' || key == 'A' || key=="ArrowLeft") && position.left >= 10) {
       let n = parseInt(left) - moveBy + "px";
       ball.style.left = n;
-    } else if ((key == 'w' || key == 'W') && position.top >= 10 ) {
+    } else if ((key == 'w' || key == 'W' || key=="ArrowUp") && position.top >= 10 ) {
       let n = parseInt(top) - moveBy + "px";
       ball.style.top = n;
     }
